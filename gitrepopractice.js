a20883f9eeeb359fbcbb0605d48b3646ce754be1
Hannah Yi <hannahyi@Hannahs-MBP.fios-router.home>
2022-05-24 19:27:12 -0400

    first commit

--- gitrepopractice.js
+++ gitrepopractice.js
@@ -1,4 +1,7 @@
-console.log('Hello');
-console.log('testing');
-console.log('yay we did it');
-console.log('babys first repo');
+// farenheit to celsius
+// celsius to farenheit {TWO BLANKS}
+// while you type it shows
+
+let userInput // document.getElementByClass('userInput').value
+const F = (userInput*1.8) + 32
+const C = (userInput - 32)*.5556
