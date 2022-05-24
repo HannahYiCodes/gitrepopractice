684cc289d9172f2de076ab0c5e295bca1f368e7f
Hannah Yi <hannahyi@Hannahs-MBP.fios-router.home>
2022-05-24 19:37:13 -0400

    first commit

--- gitrepopractice.js
+++ gitrepopractice.js
@@ -2,6 +2,17 @@
 // celsius to farenheit {TWO BLANKS}
 // while you type it shows
 
-let userInput // document.getElementByClass('userInput').value
-const F = (userInput*1.8) + 32
-const C = (userInput - 32)*.5556
+let userInputF = document.getElementById('f').value
+let userInputC = document.getElementById('c').value
+let f_output = document.getElementById('f_output').innerHTML
+let c_output = document.getElementById('c_output').innerHTML
+
+celsiusToF = () => {
+f_output.innerHTML = ((userInputF - 32)*.5556).innerHTML // f to c
+}
+celsiusToF()
+
+farenheitToC = () => {
+c_output.innerHTML = ((userInputC*1.8) + 32).innerHTML // c to f
+}
+farenheitToC()
